d04e7fa7a328c6dbf201e58c739bdfd77c5a4786
Dan Nguyen <dantheawesime24@gmail.com>
2024-05-23 11:22:57 +0200

    bullet now uses draw interface

--- Weapon/src/main/java/dk/sdu/sesp/geight/weaponsystem/bulletsystem/BulletDrawSystem.java
+++ Weapon/src/main/java/dk/sdu/sesp/geight/weaponsystem/bulletsystem/BulletDrawSystem.java
@@ -5,14 +5,13 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import dk.sdu.sesp.geight.common.data.Entity;
 import dk.sdu.sesp.geight.common.data.World;
 import dk.sdu.sesp.geight.common.services.IDrawService;
-import dk.sdu.sesp.geight.common.weapon.DefaultCanon;
 import dk.sdu.sesp.geight.common.weapon.bullet.Bullet;
 import dk.sdu.sesp.geight.common.data.entityparts.PositionPart;
 
 public class BulletDrawSystem implements IDrawService {
     @Override
     public void draw(ShapeRenderer sr, World world) {
-        for (Entity entity : world.getEntities(DefaultCanon.class)){
+        for (Entity entity : world.getEntities(Bullet.class)){
 
             PositionPart positionPart = entity.getPart(PositionPart.class);
             sr.begin(ShapeRenderer.ShapeType.Filled);
--- Weapon/src/main/java/module-info.java
+++ Weapon/src/main/java/module-info.java
@@ -1,12 +1,15 @@
+import dk.sdu.sesp.geight.common.services.IDrawService;
 import dk.sdu.sesp.geight.common.services.IEntityProcessingService;
 import dk.sdu.sesp.geight.common.services.IGamePluginService;
+import dk.sdu.sesp.geight.common.weapon.bullet.BulletSPI;
 
 module Weapon {
     exports dk.sdu.sesp.geight.weaponsystem;
     requires Common;
     requires com.badlogic.gdx;
     requires CommonWeapon;
-    requires Character;
-    requires Player;
     provides IGamePluginService with dk.sdu.sesp.geight.weaponsystem.WeaponPlugin;
+    provides IDrawService with dk.sdu.sesp.geight.weaponsystem.bulletsystem.BulletDrawSystem;
+    provides BulletSPI with dk.sdu.sesp.geight.weaponsystem.bulletsystem.BulletControlSystem;
+    provides IEntityProcessingService with dk.sdu.sesp.geight.weaponsystem.bulletsystem.BulletControlSystem;
 }
