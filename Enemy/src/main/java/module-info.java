7a93c297d13f1437aa5d5bbb26d4d2f4c54707c8
Dan Nguyen <dantheawesime24@gmail.com>
2024-05-24 20:10:27 +0200

    minor changes

--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -10,7 +10,6 @@ module Enemy {
     requires com.badlogic.gdx;
     requires Character;
     requires CommonWeapon;
-    requires Player;
     provides IGamePluginService with dk.sdu.sesp.geight.enemysystem.EnemyPlugin;
     provides IEntityProcessingService with dk.sdu.sesp.geight.enemysystem.EnemyControlSystem;
     provides IDrawService with dk.sdu.sesp.geight.enemysystem.EnemyDrawSystem;
